d3db135ab7264f6dda2c2743b523d0c3841f5664
Alexandre Bohyn <alexandre.bohyn@kuleuven.be>
2022-09-30 13:22:02 +0200

    Init commit

---
+++ R/saif.R
@@ -0,0 +1,20 @@
+#' Bail out on an event
+#'
+#' @description Generate a random excuse that Saif could have said himself.
+#' If a category is given, generate an excuse for that category specifically.
+#'
+#' @param category Optional. Category of excuse to generate.
+#' @return A string containing the excuse.
+#' @examples
+#' exc <- bail_out("sport")
+#' sprintf("I can't because %s", exc)
+#' @export
+#' @importFrom dplyr "filter"
+bail_out <- function(category=NULL){
+  if (is.null(category)){
+    subset <- excuses
+  } else{
+    subset <- filter(excuses, category == category)
+  }
+  return(subset[sample(nrow(subset), 1),]$sentence)
+}
