356735067f6906c8fa60fb0ca36d406f6b36da3f
Alexandre Bohyn <alexandre.bohyn@kuleuven.be>
2022-09-30 13:30:01 +0200

    doc for excuse dataset

---
+++ R/excuses.R
@@ -0,0 +1,17 @@
+#' Excuses
+#'
+#' @decription List of excuses that Saif has already used or that he could use in any kind
+#' of situation, along with their category of use.
+#'
+#' @details All the excuses are of the form "I + {verb} + {complement}"
+#'
+#' @docType data
+#'
+#' @usage data(excuses)
+#'
+#' @format An object of the class \code{"data.frame"}
+#' \describe{
+#'   \item{sentence}{A string containing the excuse.}
+#'   \item{category}{A string defining the category.}
+#' }
+"excuses"
